a45cbb1d0719d2d93924d8c2e825fff22737d874
unknown <rafael24_es@hotmail.com>
2025-12-29 15:36:07 -0500

    first commit

---
+++ src/lib/socket.ts
@@ -0,0 +1,6 @@
+import { io } from "socket.io-client";
+
+export const socket = io(process.env.NEXT_PUBLIC_BACKEND_API_URL as string, {
+    transports: ['websocket', 'pulling'],
+    autoConnect: false
+});
